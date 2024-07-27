00e87b856d73f085508de09af1c4c545069390ff
Sanaullah Mobini <webdevfarsi@gmail.com>
2024-07-27 21:06:05 +0430

    Game finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const generateRandomColor = () => {
+  const digits = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F'
+  ]
+  const generatedColorCode = new Array(6)
+  .fill('')
+  .map(() => digits[Math.floor(Math.random() * digits.length )]).join('')
+  return `#${generatedColorCode}`
+}
+
+const App = () => {
+  const [correctAnswer,setCorrectAnswer] = useState('')
+  const [answers,setAnswers] = useState([])
+  const [isCorrect,setIsCorrect] = useState(true)
+  const [points,setPoints] = useState(0)
+  console.log(correctAnswer, isCorrect);
+
+  const pickColor = () => {
+    const actualColor = generateRandomColor()
+    setCorrectAnswer(actualColor)
+    setAnswers([actualColor, generateRandomColor(), generateRandomColor()]
+    .sort(() => 0.5 - Math.random()))
+  }
+
+  const checkAns = (item) => {
+
+    if(item === correctAnswer){
+        setIsCorrect(true)
+        setPoints(points + 10)
+        pickColor()
+      }else{
+        setPoints(points - 10)
+        setIsCorrect(false)
+      }
+  }
+
+  useEffect(() => {
+    pickColor()
+  }, [])
+
+  // style section
+  const mainDivStyle ={
+    width : '100vw',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#b9b9b9ede'
+  }
+  const colorGameDiv = {
+    width : '400px',
+    height: '200px',
+    backgroundColor: `${correctAnswer}`
+  }
+  const buttonDiv = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop:"40px"
+  }
+  const buttons = {
+    padding:'5px 20px',
+    width:'100px',
+    outline:"none",
+  }
+
+  const wrongDiv = {
+    textAlign: "center",
+    backgroundColor: 'red',
+    color:'white',
+    marginTop:"20px"
+  }
+
 
   return (
-    <>
+    <div style={mainDivStyle}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Your point is:  {points}</div>
+        {/* color game div */}
+        <div style={colorGameDiv}></div>
+        {/* color game buttons */}
+        <div style={buttonDiv}>
+          {
+            answers.map((item,index) => {
+              return  <button onClick={() => checkAns(item)} key={index} style={buttons}> {item} </button>
+            })
+          }
+        </div>
+        {
+         !isCorrect && <div style={wrongDiv}>Wrong </div>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
